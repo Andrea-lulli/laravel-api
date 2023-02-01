09ee170eb805ec1c22cf73307434bc8b4bc33526
Andrea-Lulli <andrea.lulli89@gmail.com>
2023-02-01 16:50:16 +0100

    creata api

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,21 +1,27 @@
-es (13 sloc)  219 Bytes
-
 <template>
     <div>
+        <!-- <h1>Work in progress</h1> -->
+        <WorkInProgress />
 
-
+        <!-- Scrivere i dati dei posts -->
+        <PostsList />
     </div>
 </template>
 
 <script>
-
-import WorkInProgress from "../components/WorkInProgress.vue";
-
+import WorkInProgress from "../components/WorkInProgress.vue"
+import PostsList from "../components/posts/PostsList.vue"
 export default {
-    name: "App",
-    components:{
-        WorkInProgress
+    name: 'App',
+    components: {
+        WorkInProgress,
+        PostsList
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
     }
-
 }
 </script>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.guestLayout')
 
 @section('content')
-<h1>Front</h1>
+<div id="root">
+
+</div>
 @endsection
